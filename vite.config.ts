010d449bd1d7234d0af5b572f36978d46f16bd74
taypro12 <buingoctay1@gmail.com>
2026-01-29 10:16:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 // vite.config.ts
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    base: '/thanharuoungam/',   // ví dụ: '/ruouthanhha/'
-    plugins: [react()],
-    resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    build: { sourcemap: true }
-  };
+export default defineConfig({
+  base: '/thanharuoungam/',   // ĐỔI theo đúng tên repo đang deploy
+  plugins: [react()],
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+  build: { sourcemap: true }
 });
