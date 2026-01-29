0cdf7d5dfd35235f5c14167b7543febeec6cfcee
taypro12 <buingoctay1@gmail.com>
2026-01-29 09:14:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: '/ruouthanha/',   // ví dụ: '/ruouthanhha/'
+    base: '//',   // ví dụ: '/ruouthanhha/'
     plugins: [react()],
     resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
     define: {
